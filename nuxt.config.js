b93913ec647adbf3c418c59ab31bfb824f8afdd6
tolisdotkarag <45044471+tolisdotkarag@users.noreply.github.com>
2021-09-30 13:57:10 +0300

    Add Europe map in landing page (#1)
    * landing page with map and piecharts onclick
    
    * Europe map Added
    
    * Add toast warning for countries with no data
    
    Co-authored-by: aposkarag <aposkarag@islab.ece.ntua.gr>
    Co-authored-by: Spyros Bekiaris <spirosbekos@gmail.com>

--- nuxt.config.js
+++ nuxt.config.js
@@ -28,6 +28,11 @@ export default {
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
     '~/plugins/apis',
+    {
+      src: '~/plugins/amCharts.js',
+      ssr: false
+    }
+
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
