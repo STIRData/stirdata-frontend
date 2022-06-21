0eb12860a07f4bd7d9f8b88c3017b83e16358ad1
Anna Christaki <annachristaki@think-code.io>
2022-06-21 12:59:34 +0300

    nuxt generate params

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,8 +1,8 @@
 /* eslint-disable camelcase */
 
-import axios from 'axios';
-
 export default {
+  mode: 'universal',
+  target: 'static',
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     title: 'STIRData',
