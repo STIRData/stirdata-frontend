574711e743308037019834ede51f7b6d86601eac
Anna Christaki <annachristaki@think-code.io>
2022-11-29 11:57:58 +0200

    new page

---
+++ src/pages/profile/logout.vue
@@ -0,0 +1,26 @@
+<template>
+  <div />
+</template>
+
+<script>
+  export default {
+    middleware: 'auth',
+    layout: 'minimal',
+    created(){
+      this.logout();   
+    },
+    methods: {
+      logout() {
+          this.$auth.logout();
+          /* TODO solid logout check
+           if (this.$solid.auth) {
+            this.$solid.auth.logout()
+            .then(() => {
+              this.$store.commit('setUser', null);
+            });
+          }*/
+        }
+    }
+  }
+</script>
+
