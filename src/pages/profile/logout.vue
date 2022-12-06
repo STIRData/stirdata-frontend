71a8f08a8fdbaf16feb7a108c798e7e33ca9fe53
Anna Christaki <annachristaki@think-code.io>
2022-12-06 12:09:08 +0200

    SD-20 remove solid auth

--- src/pages/profile/logout.vue
+++ src/pages/profile/logout.vue
@@ -12,13 +12,6 @@
     methods: {
       logout() {
           this.$auth.logout();
-          /* TODO solid logout check
-           if (this.$solid.auth) {
-            this.$solid.auth.logout()
-            .then(() => {
-              this.$store.commit('setUser', null);
-            });
-          }*/
         }
     }
   }
